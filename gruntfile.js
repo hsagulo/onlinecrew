2822f350f5fd6d47bd61d5f3dcf0ccbee6be3508
Harold Lupogan <sagulo.001@gmail.com>
2015-10-17 21:35:10 +0800

    updated gruntfile.js tasks

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,112 @@
+'use strict';
 module.exports = function(grunt) {
-   
-   
+
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+
+        // watch for changes and trigger compass, jshint, uglify and livereload
+        watch: {
+            options: {
+                livereload: true,
+            },
+            compass: {
+                files: ['public/scss/**/*.{scss,sass}'],
+                tasks: ['compass']
+            },
+            js: {
+                files: '<%= jshint.all %>',
+                tasks: ['jshint', 'uglify']
+            },
+            livereload: {
+                files: ['*.html', '*.php', 'public/images/**/*.{png,jpg,jpeg,gif,webp,svg}']
+            }
+        },
+
+        // compass and scss
+        compass: {
+            dist: {
+                options: {
+                    config: 'config.rb',
+                    force: true
+                }
+            }
+        },
+
+        // javascript linting with jshint
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                "force": true
+            },
+            all: [
+                'Gruntfile.js',
+                'public/js/source/**/*.js'
+            ]
+        },
+
+        // uglify to concat, minify, and make source maps
+        uglify: {
+            dist: {
+                options: {
+                    sourceMap: 'public/js/map/source-map.js'
+                },
+                files: {
+                    'public/js/plugins.min.js': [
+                        'public/js/source/plugins.js',
+                        'public/js/vendor/**/*.js',
+                        '!public/js/vendor/modernizr*.js'
+                    ],
+                    'public/js/main.min.js': [
+                        'public/js/source/main.js'
+                    ]
+                }
+            }
+        },
+
+        // image optimization
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 7,
+                    progressive: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'public/images/',
+                    src: '**/*',
+                    dest: 'public/images/'
+                }]
+            }
+        },
+
+        // deploy via rsync
+        deploy: {
+            staging: {
+                src: "./",
+                dest: "~/path/to/theme",
+                host: "user@host.com",
+                recursive: true,
+                syncDest: true,
+                exclude: ['.git*', 'node_modules', '.sass-cache', 'Gruntfile.js', 'package.json', '.DS_Store', 'README.md', 'config.rb', '.jshintrc']
+            },
+            production: {
+                src: "./",
+                dest: "~/path/to/theme",
+                host: "user@host.com",
+                recursive: true,
+                syncDest: true,
+                exclude: '<%= rsync.staging.exclude %>'
+            }
+        }
+
+    });
+
+    // rename tasks
+    grunt.renameTask('rsync', 'deploy');
+
+    // register task
+    grunt.registerTask('default', ['watch']);
+
 };
